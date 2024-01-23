a06a5ff4dc9a68d8e2237fa89a05d0af7493f543
Furkan Berk Yılmazer <berkyilmazer@atlasyazilim.com.tr>
2024-01-23 17:03:22 +0300

    first commit created classes and  core layer

---
+++ AuthServer.Core/Repositories/IGenericRepository.cs
@@ -0,0 +1,32 @@
+﻿using AuthServer.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Core.Repositories
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<T> GetByIdAsync(int id);
+
+        Task<IEnumerable<T>> GetAllAsync();
+
+        //func ->delegate-> bir methodu işaret ediyor
+
+        IQueryable<T> Where(Expression<Func<T, bool>> predicate);
+
+        //product= productRepository.where(x=>x.id>5);
+        // product.any
+
+        //product.ToList()  -> dediğimde yukardakileri bir sorgu haline getirip veritabanına istek atar
+
+        Task AddAsync(T entity);
+
+        void Remove(T entity);
+
+        T Update(T entity);
+    }
+}
