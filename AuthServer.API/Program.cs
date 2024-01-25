f47a2e7375710e053bda52e8d11211acfeec90df
Furkan Berk Yılmazer <berkyilmazer@atlasyazilim.com.tr>
2024-01-25 09:32:52 +0300

    Added service layer

--- AuthServer.API/Program.cs
+++ AuthServer.API/Program.cs
@@ -1,11 +1,14 @@
+using Microsoft.Extensions.Configuration;
+using SharedLibrary.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.Configure<CustomTokenOption>(builder.Configuration.GetSection("TokenOption"));
 
 var app = builder.Build();
 
